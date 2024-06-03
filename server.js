da3d6a99a1d2cd48f0345276284646c084e18416
Dipesh-sapkota1 <sapkotadipes@gmail.com>
2024-06-03 07:22:21 +0545

    new router created

--- server.js
+++ server.js
@@ -3,45 +3,12 @@ const port = 5000;
 const app = express();
 
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter , a newsletter that only shres positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2033-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2033-01-02',
-  },{
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2033-01-02',
-  },
-];
-
 app.get('/', (req,res)=> {
   res.send({message: 'Welcome to the RandomIdea API'});
 });
-// Get all ideas
-app.get('/api/ideas', (req,res)=> {
-  res.json({success: true, data: ideas});
-});
 
-// Get one idea
-app.get('/api/ideas/:id', (req,res)=> {
-  const idea = ideas.find((idea)=>  idea.id === +req.params.id);
+const iddeaRouter = require('./routes/ideas');
 
-  if(!idea){
-    return res.status(404).json({success: false, error:'Resources not found'});
-  }
-  res.json({success: true, data: idea});
-});
+app.use('/api/ideas/',iddeaRouter);
 
-app.listen(port, ()=> console.log(`Serer listening 0n ${port}`));z
+app.listen(port, ()=> console.log(`Serer listening 0n ${port}`));
