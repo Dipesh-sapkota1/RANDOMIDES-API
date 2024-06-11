3f256e41baf06cb41d6f6d522c5e7ce66f4366a2
Dipesh-sapkota1 <sapkotadipes.com>
2024-06-11 10:47:07 +0545

    added frontend

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,58 +1,86 @@
 const express = require('express');
-const router  = express.Router();
-
-
-
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive NewsLetter , a newsletter that only shres positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2033-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older that your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2033-01-02',
-  },{
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-    tag: 'Software',
-    username: 'BruceBanner',
-    date: '2033-01-02',
-  },
-];
+const router = express.Router();
+const Idea = require('../models/Idea.js');
 
 // Get all ideas
-router.get('/', (req,res)=> {
-  res.json({success: true, data: ideas});
+router.get('/', async (req, res) => {
+    try {
+        const ideas = await Idea.find();
+        res.json({success: true, data: ideas});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
 });
 
 // Get one idea
-router.get('/:id', (req,res)=> {
-  const idea = ideas.find((idea)=>  idea.id === +req.params.id);
-
-  if(!idea){
-    return res.status(404).json({success: false, error:'Resources not found'});
-  }
-  res.json({success: true, data: idea});
+router.get('/:id', async (req, res) => {
+    try {
+        const idea = await Idea.findById(req.params.id);
+        res.json({success: true, data: idea});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
 });
 
-
 // Add an  idea
-router.post('/', (req,res)=> {
-  const idea = {
-    id: ideas.length + 1,
-    text: req.body.text,
-    tag: req.body.tag,
-    username: req.body.username,
-    date: new Date().toISOString().slice(0, 10),
-  }
-  ideas.push(idea);
-  res.json({success:true, data:idea});
+router.post('/', async (req, res) => {
+    const idea = new Idea({text: req.body.text, tag: req.body.tag, username: req.body.username});
+
+    try {
+        const savedIdea = await idea.save();
+        res.json({success: true, data: savedIdea});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+});
+
+// Update an idea
+router.put('/:id', async (req, res) => {
+    try {
+        const idea = await Idea.findById(req.params.id);
+
+        // Match the usernames
+        if (idea.username === req.body.username) {
+           
+            const updatedIdea = await Idea.findByIdAndUpdate(req.params.id, {
+                $set: {
+                    text: req.body.text,
+                    tag: req.body.tag
+                }
+            }, {new: true});
+        return res.json({success: true, data: updatedIdea});
+        }
+         // Usernames do not match
+         res.status(403).json({success: false, error: 'You are not authorized to update this resource'});
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
+
+});
+
+// Delete an idea
+router.delete('/:id', async (req, res) => {
+    try {
+        const idea = await Idea.findById(req.params.id);
+
+        // Match the usernames
+        if (idea.username === req.body.username) {
+            await Idea.findByIdAndDelete(req.params.id);
+           return res.json({success: true, data: {}});
+        }
+        // Usernames do not match
+        res.status(403).json({success: false, error: 'You are not authorized to delete this resource'});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, error: 'Something went wrong'});
+    }
 });
 
 
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
-const port = 5000;
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+const port = process.env.PORT;
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
+
 
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Cors middleware
+app.use(
+  cors({
+  origin: ['http://localhost:5000', 'http://localhost:3000'],
+  credentials: true,
+  }
+));
 
 app.get('/', (req,res)=> {
   res.send({message: 'Welcome to the RandomIdea API'});
